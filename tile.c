033b6733beb712b259294ef4e23c6bf1f41be524
Nicholas Hancock <nmh@Nicholass-MacBook-Pro.local>
2016-10-31 22:49:08 -0400

    Added tile rotations.

--- tile.c
+++ tile.c
@@ -19,24 +19,42 @@ struct tile create_tile(enum edge edges[5])
 	return t;
 }
 
+struct tile rotate_tile(struct tile old, int rotation)
+{
+	enum edge new[5];
+	new[4] = old.edges[4]; /* Center doesn't move. */
+	
+	for (unsigned int i = 4; i > 0; --i) { /* Need unsigned for wrapping */
+		/* Note clockwise rotation moves previous elements forward. */
+		new[i - 1] = old.edges[(i - 1 - rotation) % 4];
+	}
+	return create_tile(new);
+}
+
 void print_tile(struct tile t)
 {
+	/* Our array stores in clockwise order starting at the top.
+	 * So the tile looks like this: (indexes)
+	 * + 0 +
+	 * 3 4 1
+	 * + 2 +
+	*/
 	const char tiles[4] = {'X', 'C', 'F', 'R'};
 	printf("+%c+\n", tiles[t.edges[0]]);
-	printf("%c%c%c\n", tiles[t.edges[1]], 
-			tiles[t.edges[3]], tiles[t.edges[4]]);
+	printf("%c%c%c\n", tiles[t.edges[3]], 
+			tiles[t.edges[4]], tiles[t.edges[1]]);
 	printf("+%c+\n", tiles[t.edges[2]]);
 }
 
 struct board {
-	struct tile tiles[16];
+	struct tile tiles[4*4]; /* Arbitrary sizing */
 };
 
 struct board make_board(void)
 {
 	struct board b;
 	enum edge edges[5] = { NONE, NONE, NONE, NONE, NONE };
-	for (int i = 0; i < 16; ++i) {
+	for (int i = 0; i < 4*4; ++i) {
 		b.tiles[i] = create_tile(edges);
 	}
 	return b;
@@ -70,6 +88,10 @@ int main(void)
 	printf("\nMixed tile: \n");
 	enum edge mixed[5] = { CITY, FIELD, ROAD, CITY, ROAD };
 	print_tile(create_tile(mixed));
+	for (int i = 1; i <= 4; ++i) {
+		printf("\nRotate mixed tile %d: \n", i);
+		print_tile(rotate_tile(create_tile(mixed), i));
+	}
 
 	printf("\nTesting board creation. All Null.\n");
 	struct board b = make_board();
